bda810a15413c5fff5423654f8293a8a1c5e3428
xPer4 <gregory_2004@icloud.com>
2023-10-26 09:18:19 +0300

    updated Task_10 && undated 13_5

--- src/Task_10/SortingStudentsByGPA.java
+++ src/Task_10/SortingStudentsByGPA.java
@@ -3,6 +3,7 @@ package Task_10;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
 
 public class SortingStudentsByGPA {
     private ArrayList<Student> students = new ArrayList<>();
@@ -27,9 +28,50 @@ public class SortingStudentsByGPA {
         }
     }
 
-    public void sortStudentsByField(Comparator<Student> comparator) {
-        Collections.sort(students, comparator);
+    public void sortStudentsByField(List<Student> students, Comparator<Student> comparator) {
+        mergeSort(students, comparator);
     }
+
+    private void mergeSort(List<Student> students, Comparator<Student> comparator) {
+        if (students.size() <= 1) {
+            return;
+        }
+
+        int middle = students.size() / 2;
+        List<Student> left = new ArrayList<>(students.subList(0, middle)); // Создаем копии подсписков
+        List<Student> right = new ArrayList<>(students.subList(middle, students.size()));
+
+        mergeSort(left, comparator);
+        mergeSort(right, comparator);
+
+        merge(students, left, right, comparator);
+    }
+
+    private void merge(List<Student> students, List<Student> left, List<Student> right, Comparator<Student> comparator) {
+        int leftIndex = 0;
+        int rightIndex = 0;
+        int resultIndex = 0;
+
+        while (leftIndex < left.size() && rightIndex < right.size()) {
+            if (comparator.compare(left.get(leftIndex), right.get(rightIndex)) < 0) {
+                students.set(resultIndex++, left.get(leftIndex++));
+            } else {
+                students.set(resultIndex++, right.get(rightIndex++));
+            }
+        }
+
+        while (leftIndex < left.size()) {
+            students.set(resultIndex++, left.get(leftIndex++));
+        }
+
+        while (rightIndex < right.size()) {
+            students.set(resultIndex++, right.get(rightIndex++));
+        }
+    }
+
+//    public void sortStudentsByField(Comparator<Student> comparator) {
+//        Collections.sort(students, comparator);
+//    }
 
     public static void main(String[] args) {
         SortingStudentsByGPA sorter = new SortingStudentsByGPA();
@@ -50,7 +92,7 @@ public class SortingStudentsByGPA {
         sorter.outArray();
 
         // Sorting by another field, e.g., first name
-        sorter.sortStudentsByField(Comparator.comparing(Student::getFirstName));
+        sorter.sortStudentsByField(studentsList,Comparator.comparing(Student::getFirstName));
 
         System.out.println("\nSorted by First Name:");
         sorter.outArray();
--- src/Task_13/Task_13_5/PhoneNumberConverter.java
+++ src/Task_13/Task_13_5/PhoneNumberConverter.java
@@ -3,37 +3,54 @@ package Task_13.Task_13_5;
 
 public class PhoneNumberConverter {
     public static String formatPhoneNumber(String input) {
-        // Удаляем все пробелы и дефисы, если они есть
-        String cleanedInput = input.replaceAll("[\\s-]", "");
+        // Удаляем все символы, кроме цифр
+        String digits = input.replaceAll("\\D", "");
 
-        // Проверяем, в каком формате задан номер и преобразуем его
-        if (cleanedInput.startsWith("+")) {
-            // Формат +<Код страны><Номер 10 цифр>
-            if (cleanedInput.length() == 12 && cleanedInput.substring(1).matches("\\d+")) {
-                String countryCode = cleanedInput.substring(1, 4);
-                String firstPart = cleanedInput.substring(4, 7);
-                String secondPart = cleanedInput.substring(7);
-                return "+" + countryCode + "-" + firstPart + "-" + secondPart;
-            }
-        } else if (cleanedInput.startsWith("8") && cleanedInput.length() == 11) {
-            // Формат 8<Номер 10 цифр> для России
-            String firstPart = cleanedInput.substring(1, 4);
-            String secondPart = cleanedInput.substring(4, 7);
-            String thirdPart = cleanedInput.substring(7);
-            return "+7" + "-" + firstPart + "-" + secondPart + "-" + thirdPart;
+        // Проверяем, в каком формате передан номер
+        if (digits.startsWith("8")) {
+            // Добавляем +7 для России
+            digits = "7" + digits.substring(1);
+        } else if (digits.startsWith("+")) {
+            // Удаляем символ "+" в начале строки
+            digits = digits.substring(1);
         }
 
-        // Если ни один из форматов не подходит, вернуть исходную строку
-        return input;
+        // Проверяем, что у нас достаточно цифр для форматирования
+        if (digits.length() >= 10) {
+            // Форматируем номер
+            String countryCode = digits.substring(0, digits.length() - 10);
+            String firstPart = digits.substring(digits.length() - 10, digits.length() - 7);
+            String secondPart = digits.substring(digits.length() - 7, digits.length() - 4);
+            String thirdPart = digits.substring(digits.length() - 4);
+
+            return "+" + countryCode + "-" + firstPart + "-" + secondPart + "-" + thirdPart;
+        } else {
+            // Если цифр недостаточно, вернем исходную строку
+            return input;
+        }
     }
 
     public static void main(String[] args) {
+        System.out.println("Было:");
         String phoneNumber1 = "+79175655655";
         String phoneNumber2 = "+104289652211";
         String phoneNumber3 = "89175655655";
+        String phoneNumber4 = "+71113334445555";
 
-        System.out.println(formatPhoneNumber(phoneNumber1)); // +7-917-565-5655
-        System.out.println(formatPhoneNumber(phoneNumber2)); // +104-289-652-211
-        System.out.println(formatPhoneNumber(phoneNumber3)); // +7-917-565-5655
+        System.out.println(phoneNumber1);
+        System.out.println(phoneNumber2);
+        System.out.println(phoneNumber3);
+        System.out.println(phoneNumber4);
+
+
+        String formatted1 = formatPhoneNumber(phoneNumber1);
+        String formatted2 = formatPhoneNumber(phoneNumber2);
+        String formatted3 = formatPhoneNumber(phoneNumber3);
+        String formatted4 = formatPhoneNumber(phoneNumber4);
+        System.out.println("Стало:");
+        System.out.println(formatted1);
+        System.out.println(formatted2);
+        System.out.println(formatted3);
+        System.out.println(formatted4);
     }
 }
